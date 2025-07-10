de59ef7ba82cb5ec2399599eeca5c359d13ce8a8
ElPengu <edwin.nganga2003@gmail.com>
2025-07-10 06:17:21 +0100

    New area to play around with ASP.NET Core

---
+++ ASPNETPRACTISE_EDWIN/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPNETPRACTISE_EDWIN.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
