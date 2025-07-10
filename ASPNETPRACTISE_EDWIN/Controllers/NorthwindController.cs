0b2ba80b4bc939ff6e4d4eca94037563c8d8b1d5
ElPengu <edwin.nganga2003@gmail.com>
2025-07-10 07:00:23 +0100

    Setting up play area with area for Northwind database learning

---
+++ ASPNETPRACTISE_EDWIN/Controllers/NorthwindController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETPRACTISE_EDWIN.Controllers
+{
+    public class NorthwindController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
